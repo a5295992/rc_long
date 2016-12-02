e03b14e62a1e53f77097088d18a0fd12e31a66d6
zhou-xiangl <zhou-xiangl@MS-20161103WDFD>
2016-12-02 11:51:31 +0800

    12.2日提交 sqlHelper完善

--- src/com/rc_long/dao/SqlCreate.java
+++ src/com/rc_long/dao/SqlCreate.java
@@ -1,18 +1,19 @@
 package com.rc_long.dao;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class SqlCreate {
 	/**
 	 * 查询所有
 	 * @param clazz
 	 * @return 1
 	 */
-	public static StringBuilder generateQuerySql(Class<?> clazz,StringBuilder sb){
-		String tableName=clazz.getSimpleName();
-		tableName=deleWithTableName(tableName, sb);
-		
+	public static void generateQuerySql(Class<?> clazz,StringBuilder sb){
 		sb.append("select *from ");
-		sb.append(tableName);
-		return sb;
+		String tableName=clazz.getSimpleName();
+		//将class名字变为 table 名
+		deleWithTableName(tableName, sb);
 	}
 	/**
 	 * 查询一个或者多个
@@ -20,62 +21,76 @@ public class SqlCreate {
 	 * @param obj
 	 * @return 2
 	 */
-	public static StringBuilder generateQuerySql(Class<?> clazz,String []obj,StringBuilder sb){
+	public static void generateQuerySql(Class<?> clazz,String []obj,StringBuilder sb){
+		sb.append("select ");
 		String tableName=clazz.getSimpleName();
 		//先将其首字母变为小写 添加
-		tableName=deleWithTableName(tableName, sb);
-		
-		sb.append("select ");
 		for (int i = 0; i < obj.length; i++) {
 			sb.append(obj[i]);
-			sb.append(",");
+			if(obj.length-i>1){
+				sb.append(",");
+			}
 		}
 		sb.append(" from ");
-		sb.append(tableName);
-		return sb;
+		deleWithTableName(tableName, sb);
 	}
 	/**
 	 * 查询数量
 	 * @return
 	 */
-	public static StringBuilder generateQueryCountSql(Class<?> clazz,StringBuilder sb,String obj[]){
+	public static void generateQueryCountSql(Class<?> clazz,StringBuilder sb,Map<String, Object> obj){
 		String tableName=clazz.getSimpleName();
-		//先将其首字母变为小写 添加
-		tableName=deleWithTableName(tableName, sb);
 		sb.append("select ");
 		parseArray(obj, sb);
 		sb.append(" count(*) from ");
-		sb.append(tableName);
-		return sb;
+		//先将其首字母变为小写 添加
+		deleWithTableName(tableName, sb);
 	}
+	/**
+	 * 生成 关联查询语句
+	 * @param sb
+	 * @param classArr
+	 * @param condition
+	 */
+	public static void genenrateLeftJoinQuerySql(Map<String, Object> obj){
+		StringBuilder sbx=new StringBuilder();
+		
+	}
+	
 	/**
 	 * 添加条件 生成预编译语句
 	 * @param obj
 	 * @param sb
 	 */
-	public static StringBuilder setConditions(String []obj,StringBuilder sb){
+	public static void setConditions(Map<String,Object>map,StringBuilder sb){
 		sb.append(" where ");
-		parseArray(obj,sb);
-		return sb;
+		parseArray(map,sb);
 	}
 	//用来解析数组 组装成为语句
-	public static StringBuilder parseArray(String []obj,StringBuilder sb){
-		for (int i = 0; i < obj.length; i++) {
-			sb.append(obj[i]);
+	public static void parseArray(Map<String, Object> map,StringBuilder sb){
+		int index=0;
+		for(Entry<String, Object> m:map.entrySet()){
+			sb.append(m.getKey());
 			sb.append(" = ");
-			sb.append(" ? ");
-			if(obj.length-i>1){
+			sb.append("'"+m.getValue()+"'");
+			if(map.size()-index>1){
 				sb.append(" and ");
 			}
+			index++;
 		}
-		return sb;
 	}
-	public static StringBuilder OrderBy(String culum,String desc,StringBuilder sb){
+	/**
+	 * 排序
+	 * @param culum
+	 * @param desc
+	 * @param sb
+	 * @return
+	 */
+	public static void OrderBy(String culum,String desc,StringBuilder sb){
 		sb.append(" ORDER BY ");
 		sb.append(culum);
 		sb.append(" ");
 		sb.append(desc);
-		return sb;
 	}
 	//将实体名替换成相应的表名
 	public static String deleWithTableName(String tableName,StringBuilder sbx){
@@ -91,14 +106,49 @@ public class SqlCreate {
 		}
 		return sbx.toString();
 	}
-	public static StringBuilder groupBy(StringBuilder sb,String sbx){
+	/**
+	 * 分组
+	 * @param sb
+	 * @param sbx
+	 * @return
+	 */
+	public static void groupBy(StringBuilder sb,String sbx){
 		sb.append(" group by ");
 		sb.append(sbx);
-		return sb;
 	}
-	public static StringBuilder having(String [] obj,StringBuilder sb){
+	/**
+	 * 分组后条件
+	 * @param obj
+	 * @param sb
+	 * @return
+	 */
+	public static  void having(Map<String, Object> obj,StringBuilder sb){
 		sb.append(" having ");
 		parseArray(obj, sb);
-		return sb;
 	}
+	/**
+	 * 分页查询
+	 * @param sb 放在最后
+	 * @return
+	 */
+	public static void limitIn(StringBuilder sb,int start,int end){
+		sb.append(" limit ");
+		sb.append(start);
+		sb.append(',');
+		sb.append(end);
+		sb.append(" ");
+	}
+	
+	public static void like(StringBuilder sb,String clum,Object like){
+		if(sb.toString().contains("where")){
+			sb.append(" and ");
+		}else{
+			sb.append(" where ");
+		}
+		sb.append(clum);
+		sb.append(" like ");
+		sb.append("'"+'%'+""+like+""+'%'+"'");
+	}
+	
+	
 }
